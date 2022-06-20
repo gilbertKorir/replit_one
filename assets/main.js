4508be4c63bc0c4a0950296ab0fb49e4aee5c0ce
gilbertKorir <jerikorir@gmail.com>
2022-06-20 17:45:15 +0300

    added a js functionality

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+
+function getName(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myname").value;
+    
+    // Displaying the value
+    document.getElementById("display").innerHTML= "Hello " + inputVal + ", welcome to the bootcamp"
+
+}
